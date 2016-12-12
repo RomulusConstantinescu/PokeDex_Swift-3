5ba552c399cc000b0663c4d43e649d80a8a4e2fb
Romulus Constantinescu <romulusc@hotmail.co.uk>
2016-12-12 19:23:06 +0000

    Initial Commit

---
+++ PokeDex_Swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeDex_Swift3
+//
+//  Created by Romulus Constantinescu on 12/12/2016.
+//  Copyright © 2016 Romulus Constantinescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
